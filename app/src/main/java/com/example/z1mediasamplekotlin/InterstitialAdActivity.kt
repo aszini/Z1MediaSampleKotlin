682d77dbbae7939cc8756dabd97f80f3fda1f4e9
asaini <asaini@z1media.com>
2023-02-28 11:47:07 +0530

    initial commit

---
+++ app/src/main/java/com/example/z1mediasamplekotlin/InterstitialAdActivity.kt
@@ -0,0 +1,38 @@
+package com.example.z1mediasamplekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.z1media.android.sdk.Z1InterstitialAd
+import com.z1media.android.sdk.listeners.Z1AdManagerInterstitialI
+
+class InterstitialAdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interstitial_ad)
+
+        val interstitialAd =  Z1InterstitialAd.Builder(this)
+            .setTagName("in-app-sample-in-Z1")
+            .setZ1AdManagerInterstitialAdLoadCallback( object : Z1AdManagerInterstitialI {
+                override fun onAdFailedToLoad(errorMessage: String) {
+                }
+
+                override fun onAdClicked() {
+                }
+
+                override fun onAdDismissedFullScreenContent() {
+                }
+
+                override fun onAdFailedToShowFullScreenContent(errMsg: String) {
+                }
+
+                override fun onAdImpression() {
+                }
+
+                override fun onAdShowedFullScreenContent() {
+
+                }
+            }).build()
+        interstitialAd.loadInterstitialAd()
+    }
+}
---
+++ app/src/main/java/com/example/z1mediasamplekotlin/RewardedVideoActivity.kt
@@ -0,0 +1,46 @@
+package com.example.z1mediasamplekotlin
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.z1mediasamplekotlin.databinding.ActivityRewardedVideoBinding
+import com.z1media.android.sdk.Z1RewardedVideoAd
+import com.z1media.android.sdk.listeners.Z1RewardedVideoI
+
+class RewardedVideoActivity : AppCompatActivity() {
+    lateinit var  binding: ActivityRewardedVideoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRewardedVideoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val rewardedVideoAd = Z1RewardedVideoAd.Builder(this)
+            .setTagName("in-app-sample-rw-Z1")
+            .setZ1RewardInterstitialI(object : Z1RewardedVideoI {
+
+                override fun onAdFailedToLoad(errorMessage: String) {
+                }
+
+                override fun onAdClicked() {
+                }
+
+                override fun onAdDismissedFullScreenContent() {
+                }
+
+                override fun onAdFailedToShowFullScreenContent(errMsg: String) {
+                }
+
+                override fun onAdImpression() {
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                }
+
+                override fun onUserEarnedReward(amount: Int, type: String) {
+                    binding.title.text = "Reward Amount $amount , type: $type"
+                }
+
+            }).build()
+        rewardedVideoAd.loadRewardVideoAd()
+    }
+}
